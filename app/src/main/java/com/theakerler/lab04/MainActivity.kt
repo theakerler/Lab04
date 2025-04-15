84286206a00af173d5c185d1f1d749e9c031a923
Alexander EP <palex2143@gmail.com>
2025-04-15 16:21:18 -0500

    Add ViewHolaCurso

--- app/src/main/java/com/theakerler/lab04/MainActivity.kt
+++ app/src/main/java/com/theakerler/lab04/MainActivity.kt
@@ -28,28 +28,52 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab04Theme {
-                MiPantalla()
+//                MiPantalla()
             }
         }
     }
 }
 
-@Composable
-fun MiPantalla() {
-    var mostrarTexto by remember { mutableStateOf(false) }
 
-    LazyColumn {
-        item {
-            FloatingActionButton(onClick = {
-                mostrarTexto = true
-            }) {
-                Icon(Icons.Default.Add, contentDescription = "Añadir")
-            }
+fun ViewHolaCurso() {
+    Column(
+        modifier = Modifier
+            .fillMaxWith()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Welcome to the Course!",
+            fontSize = 28.sp,
+            fontWeight = FontWeigh.Bold
+        )
+        Spacer(modifier = Modifier.heigh(16.dp))
+        Text(
+            text = "Hello, Student!",
+            fontSize = 20.xD
 
-            if (mostrarTexto) {
-                Text("Hola mundo")
-            }
-        }
     }
 }
 
+
+
+
+//@Composable
+//fun MiPantalla() {
+//    var mostrarTexto by remember { mutableStateOf(false) }
+//
+//    LazyColumn {
+//        item {
+//            FloatingActionButton(onClick = {
+//                mostrarTexto = true
+//            }) {
+//                Icon(Icons.Default.Add, contentDescription = "Añadir")
+//            }
+//
+//            if (mostrarTexto) {
+//                Text("Hola mundo")
+//            }
+//        }
+//    }
+//}
+//
