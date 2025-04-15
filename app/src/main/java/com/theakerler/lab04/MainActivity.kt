04962bbdd56e703ea367ed9299f7d73969e4bc94
Alexander EP <palex2143@gmail.com>
2025-04-15 16:53:38 -0500

    Añadido y estilizado el tercer componente: ListaDeMensajes

--- app/src/main/java/com/theakerler/lab04/MainActivity.kt
+++ app/src/main/java/com/theakerler/lab04/MainActivity.kt
@@ -8,12 +8,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -31,6 +34,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.theakerler.lab04.ui.theme.Lab04Theme
+import androidx.compose.foundation.lazy.items
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -82,4 +86,32 @@ fun MiPantalla() {
     }
 }
 
+@Composable
+fun ListaDeMensajes() {
+    val mensajes = listOf("Mensaje 1", "Mensaje 2", "Mensaje 3")
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF0F0F0))
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(items = mensajes) { mensaje ->
+            Card(
+                elevation = CardDefaults.cardElevation(4.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = mensaje,
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(16.dp),
+                    color = Color(0xFF333333)
+                )
+            }
+        }
+    }
+}
 
